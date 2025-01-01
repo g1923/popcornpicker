07a9b5dfa0d07fac4788b01d1ff5703cbf7aea5a
G1 <lovyellow923@naver.com>
2025-01-02 02:53:54 +0900

    Initial commit

---
+++ src/main/java/com/g1/popcornpicker/PopcornpickerApplication.java
@@ -0,0 +1,13 @@
+package com.g1.popcornpicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopcornpickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopcornpickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g1/popcornpicker/PopcornpickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g1.popcornpicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopcornpickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
